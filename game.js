dba3b7a07aeebfdaa2d21d03330e42193b46d45a
Felipe Ramos <perenecabuto@gmail.com>
2011-11-23 22:55:34 -0200

    Colisão

--- game.js
+++ game.js
@@ -1,32 +1,46 @@
+var game;
+
 function createNewGame() {
-    new Game().init();
+    game = new Game();
+    game.init();
 };
 
 function Game() {
   var _self = this;
 
   this.objects = [];
-  this.playerObject = null;
+  this.player = null;
 
   this.init = function() {
     this.canvas = document.getElementById('board');
     this.context = this.canvas.getContext('2d');
+    this.objectManager = new ObjectManager( this.context, this.canvas);
     this.createPlayerObject();
     this.preparePlayerEvents();
+    this.createChallenger();
+    this.createChallenger();
     this.drawScreen();
   };
 
   this.createPlayerObject = function() {
     var fighter = new Fighter();
-    this.playerObject = fighter;
-    this.playerObject.x = this.canvas.width / 2;
-    this.playerObject.y = this.canvas.height / 2;
+    this.player = fighter;
+    this.player.x = this.canvas.width / 2;
+    this.player.y = this.canvas.height / 2;
+    this.addObject(fighter);
+  };
+
+  this.createChallenger = function() {
+    var fighter = new Fighter();
+    fighter.x = Math.round(Math.random() * game.canvas.width);
+    fighter.y = Math.round(Math.random() * game.canvas.height);
+    fighter.angle = Math.round(Math.random() * 360);
     this.addObject(fighter);
   };
 
   this.preparePlayerEvents = function() {
     window.addEventListener('keyup', function(e) {
-      _self.playerObject.stop();
+      _self.player.stop();
     });
 
     window.addEventListener('keydown', function(e) {
@@ -35,30 +49,30 @@ function Game() {
           return false;
       }
 
-      _self.playerObject.move();
+      _self.player.move();
 
       switch(e.keyCode) {
         case 40:
         case 83:
-          _self.playerObject.moveDown();
+          _self.player.moveBackward();
           break;
         case 38:
         case 87:
-          _self.playerObject.moveUp();
+          _self.player.moveForward();
           break;
         case 37:
         case 65:
-          _self.playerObject.moveLeft();
+          _self.player.moveLeft();
           break;
         case 39:
         case 68:
-          _self.playerObject.moveRight();
+          _self.player.moveRight();
           break;
         case 32:
-          _self.playerObject.punch();
+          _self.player.punch();
           break;
         default:
-          _self.playerObject.stop();
+          _self.player.stop();
       };
     });
   };
@@ -73,8 +87,12 @@ function Game() {
 
     for (i in _self.objects) {
         var object = _self.objects[i];
-        object.update(_self.context, _self.canvas);
+        _self.objectManager.update(object);
     }
+
+    _self.objectManager.updateForCollision(_self.objects);
+
+    _self.objectManager.showInfo(_self.player);
 
     setTimeout(_self.drawScreen, 150);
   };
@@ -84,17 +102,67 @@ function Game() {
   };
 };
 
-var angle = 0;
+function ObjectManager(context, canvas) {
+
+  this.context = context;
+  this.canvas = canvas;
+
+  this.update = function(object) {
+    this.context.save();
+
+    object.x = object.x < 0 ? 0 : (object.x > this.canvas.width  ? this.canvas.width  : object.x);
+    object.y = object.y < 0 ? 0 : (object.y > this.canvas.height ? this.canvas.height : object.y);
+
+    this.context.translate(object.x + (object.size/2), object.y + (object.size/2));
+    this.context.rotate(object.angle * (Math.PI / 180));
+    this.context.drawImage(object.getImage(), 0, 0, object.size * 2, object.size * 2, -object.size/2, -object.size/2, object.size, object.size);
+
+    this.context.restore();
+  };
+
+  this.updateForCollision = function(objects) {
+    for (i in objects) {
+      var object = objects[i];
+      var nextStep = {x: object.getNextXStep(), y: object.getNextYStep()};
+
+      for (i in objects) {
+        var currObj = objects[i];
+        if (object != currObj && this.getDistance(object, currObj) <= object.size / 2) {
+          object.move();
+          object.moveBackward();
+          object.stop();
+        }
+      }
+    }
+  };
+
+  this.getDistance = function(obj1, obj2) {
+    return Math.sqrt(Math.pow(obj1.x - obj2.x, 2) + Math.pow(obj1.y - obj2.y, 2));
+  };
+
+  this.showInfo = function(object) {
+    var angle = object.angle;
+    var dimensions = [Math.round(object.x), Math.round(object.y)];
+
+    this.context.font = '10px sans-serif';
+    this.context.strokeText("Ang: " + angle + " (X: " + dimensions[0] + " Y: " + dimensions[1] + ")", 10, 10);
+    this.context.strokeText("SETAS para andar, ESPACO para Bater =)", 60, 145);
+  };
+}
+
+
 function Fighter() {
   var STOPPED  = 1;
   var MOVING   = 2;
   var PUNCHING = 3;
+  var DANCING  = 4;
+  var DEAD     = 0;
 
   this.x = 0;
   this.y = 0;
-  this.rotation = 0;
+  this.angle = 0;
   this.stepSize = 10;
-  this.rotationStep = 45;
+  this.angleStep = 45;
   this.size = 32;
   this.state = STOPPED;
 
@@ -116,23 +184,6 @@ function Fighter() {
   this.images.punch_left.src = 'player_punch_l.png';
   this.images.punch_right.src = 'player_punch_r.png';
 
-  this.update = function(context, canvas) {
-    context.save();
-
-    this.x = this.x < 0 ? 0 : (this.x > canvas.width  ? canvas.width  : this.x);
-    this.y = this.y < 0 ? 0 : (this.y > canvas.height ? canvas.height : this.y);
-
-
-    context.translate(this.x + (this.size/2), this.y + (this.size/2));
-    context.rotate(this.rotation * (Math.PI / 180));
-    context.drawImage(this.getImage(), 0, 0, this.size * 2, this.size * 2, -this.size/2, -this.size/2, this.size, this.size);
-
-    context.restore();
-
-    context.font = '10px sans-serif';
-    context.strokeText("Ang: " + this.rotation + " (X: " + Math.round(this.x) + " Y: " + Math.round(this.y) + ")", 10, 10);
-    context.strokeText("SETAS para andar, ESPACO para Bater =)", 60, 145);
-  };
 
   this.isStopped = function() {
     return this.state == STOPPED;
@@ -155,11 +206,11 @@ function Fighter() {
   }
 
 
-  this.moveUp = function() {
+  this.moveForward = function() {
     this.step(false);
   };
 
-  this.moveDown = function() {
+  this.moveBackward = function() {
     this.step(true)
   };
 
@@ -171,23 +222,30 @@ function Fighter() {
     this.rotate(true);
   };
 
-
   this.step = function(reverse) {
     reverse = reverse || false;
     mult = reverse ? -1 : +1;
 
     if (!this.isStopped()) {
-      this.y -= this.getDirection(reverse) * this.stepSize * Math.cos((Math.PI / 180) * this.rotation);
-      this.x += this.getDirection(reverse) * this.stepSize * Math.sin((Math.PI / 180) * this.rotation);
+      this.y += this.getDirection(reverse) * this.getNextYStep();
+      this.x += this.getDirection(reverse) * this.getNextXStep();
     }
   };
 
+  this.getNextYStep = function() {
+    return -1 * this.stepSize * Math.cos((Math.PI / 180) * this.angle);
+  };
+
+  this.getNextXStep = function() {
+    return this.stepSize * Math.sin((Math.PI / 180) * this.angle)
+  }
+
   this.rotate = function(reverse) {
     if (!this.isStopped()) {
-      this.rotation += this.getDirection(reverse) * this.rotationStep;
+      this.angle += this.getDirection(reverse) * this.angleStep;
 
-      if (this.rotation >= 360 || this.rotation < -360) {
-        this.rotation = 0;
+      if (this.angle >= 360 || this.angle < -360) {
+        this.angle = 0;
       }
     }
   };
